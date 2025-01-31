24bcec225fe9adb3cf5b6cd8f538b5a8a6177c04
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2025-02-01 00:10:48 +0530

    Added test cases and validated

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -1,10 +1,7 @@
 package Academy;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,10 +16,10 @@ public class BrowserTest {
 		System.out.println("Hello Guys");
 		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 		WebDriver driver =new ChromeDriver();
-		driver.get("http://localhost:8080/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
+		driver.get("http://localhost:8081/MyWebApp/");
+		String text =driver.getTitle();
 		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("CodersArcade.com Learning"));
+		Assert.assertTrue(text.equalsIgnoreCase("Coders Arcade - CI/CD Learning"));
 		driver.close();
 
 	}
